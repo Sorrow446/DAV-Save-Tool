ded003b98deb1d5b6f8b76e20843a03db1e1a171
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2024-11-25 19:20:49 +0000

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(403, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(398, 169);
+            this.panel2.TabIndex = 1;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(398, 169);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(393, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open saves directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(405, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(393, 31);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Inject";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitter1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter1.Location = new System.Drawing.Point(0, 174);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(800, 39);
+            this.splitter1.TabIndex = 4;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 213);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitter1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DAVST inject GUI";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private ToolTip toolTip1;
+        private ToolTip toolTip2;
+        private Splitter splitter1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using DarkModeForms;
+using Label = System.Windows.Forms.Label;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string fullInPath = string.Empty;
+        private string fullOutPath = string.Empty;
+        private string davstPath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomUI();  
+
+            _ = new DarkModeCS(this);
+
+            davstPath = Path.Combine(Application.StartupPath, "davst.exe");
+
+            if (!File.Exists(davstPath))
+            {
+                    
+                Messenger.MessageBox(
+                    $"DAVST binary is missing: {davstPath}",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                this.Close();
+            }
+
+        }
+
+        private void InitializeCustomUI()
+        {
+            Label label1 = new Label();
+            label1.Name = "label1";
+            label1.AutoSize = true;
+
+            label1.Text = "Drag source save file here.";
+
+            Size textSize = TextRenderer.MeasureText(label1.Text, label1.Font);
+            label1.Location = new Point(
+                (panel1.Width - textSize.Width) / 2,
+                (panel1.Height - textSize.Height) / 2
+            );
+
+
+            label1.MouseHover += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(fullInPath))
+                {
+                    toolTip1.SetToolTip(label1, fullInPath);
+                }
+            };
+
+            panel1.Controls.Add(label1);
+
+
+            Label label2 = new Label();
+            label2.Name = "label1";
+            label2.AutoSize = true;
+            label2.Text = "Drag destination save file here.";
+
+            textSize = TextRenderer.MeasureText(label2.Text, label2.Font);
+
+
+
+            label2.Location = new Point(
+                (panel2.Width - textSize.Width) / 2,
+                (panel2.Height - textSize.Height) / 2
+            );
+
+            label2.MouseHover += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(fullOutPath))
+                {
+                    toolTip2.SetToolTip(label2, fullOutPath);
+                }
+            };
+
+            panel2.Controls.Add(label2);
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (paths.Length > 1)
+                {
+                    e.Effect = DragDropEffects.None;
+                    Cursor.Current = Cursors.No;
+                }
+                else
+                {
+                    FileInfo fileInfo = new FileInfo(paths[0]);
+
+                    if (fileInfo.Exists && fileInfo.Extension == ".csav")
+                    {
+                        e.Effect = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effect = DragDropEffects.None;
+                        Cursor.Current = Cursors.No;
+                    }
+                }
+
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // Show no-drop cursor
+            }
+        }
+
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] paths = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (paths.Length > 1)
+                {
+                    e.Effect = DragDropEffects.None;
+                    Cursor.Current = Cursors.No;
+                }
+                else
+                {
+                    FileInfo fileInfo = new FileInfo(paths[0]);
+
+                    if (fileInfo.Exists && fileInfo.Extension == ".csav")
+                    {
+                        e.Effect = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effect = DragDropEffects.None;
+                        Cursor.Current = Cursors.No;
+                    }
+                }
+
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files[0] == fullOutPath)
+            {
+                Messenger.MessageBox(
+                    "The source and destination paths can't be the same.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+
+            FileInfo fileInfo = new FileInfo(files[0]);
+
+            if (!fileInfo.Exists)
+            {
+                return;
+            }
+
+            string filename = fileInfo.Name;
+            Label label1 = (Label)panel1.Controls["label1"];
+            label1.Text = filename;
+
+            Size textSize = TextRenderer.MeasureText(label1.Text, label1.Font);
+
+            label1.Location = new Point(
+                (panel1.Width - textSize.Width) / 2,
+                (panel1.Height - textSize.Height) / 2
+            );
+
+            fullInPath = files[0];
+
+            if (!string.IsNullOrEmpty(fullOutPath) && !button3.Enabled)
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files[0] == fullInPath)
+            {
+                Messenger.MessageBox(
+                    "The source and destination paths can't be the same.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            FileInfo fileInfo = new FileInfo(files[0]);
+
+            if (!fileInfo.Exists)
+            {
+                return;
+            }
+
+            string filename = fileInfo.Name;
+            Label label2 = (Label)panel2.Controls["label1"];
+            label2.Text = filename;
+
+            Size textSize = TextRenderer.MeasureText(label2.Text, label2.Font);
+
+            label2.Location = new Point(
+                (panel1.Width - textSize.Width) / 2,
+                (panel1.Height - textSize.Height) / 2
+            );
+
+            fullOutPath = files[0];
+
+            if (!string.IsNullOrEmpty(fullInPath) && !button3.Enabled)
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+                string directoryPath = Path.Combine(userProfile, @"Documents\BioWare\Dragon Age The Veilguard\save games");
+
+                if (Directory.Exists(directoryPath))
+                {
+                    Process.Start("explorer.exe", directoryPath);
+                }
+                else
+                {
+                    MessageBox.Show("Directory not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening the directory: " + ex.Message);
+            }
+        }
+
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C \"\"{davstPath}\" ia -i \"{fullInPath}\" -o \"{fullOutPath}\"\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                StandardOutputEncoding = Encoding.UTF8,
+                StandardErrorEncoding = Encoding.UTF8
+            };
+
+            try
+            {
+                using (Process process = Process.Start(startInfo))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string errorOutput = process.StandardError.ReadToEnd();
+
+                    process.WaitForExit();
+
+                    string allOutput = output + Environment.NewLine + errorOutput;
+                    ShowOutputWindow(allOutput);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ShowOutputWindow(string message)
+        {
+            string formattedMessage = message.Replace("\n", Environment.NewLine);
+
+            Form outputForm = new Form
+            {
+                Text = "DAVST output",
+                Size = new Size(500, 300),
+                StartPosition = FormStartPosition.CenterScreen,
+                MaximizeBox = false,
+                FormBorderStyle = FormBorderStyle.Fixed3D,
+                Icon = this.Icon,
+            };
+
+            TextBox outputTextBox = new TextBox
+            {
+                Multiline = true,
+                Dock = DockStyle.Fill,
+                ScrollBars = ScrollBars.Vertical,
+                Text = formattedMessage,
+                ReadOnly = true,
+                BackColor = Color.Black,
+                ForeColor = Color.LightGray,
+            };
+
+            outputTextBox.SelectionStart = outputTextBox.Text.Length;
+            outputTextBox.SelectionLength = 0;
+
+            Button closeButton = new Button
+            {
+                Text = "Exit",
+                Dock = DockStyle.Bottom,
+                Height = 40,
+                Font = new Font("Verdana", 11.25F, FontStyle.Regular, GraphicsUnit.Point),
+                TabIndex = 2,
+                UseVisualStyleBackColor = true,
+
+            };
+
+            closeButton.Click += (sender, e) =>
+            {
+                Application.Exit();
+            };
+
+            _ = new DarkModeCS(outputForm);
+
+            outputForm.Controls.Add(outputTextBox);
+            outputForm.Controls.Add(closeButton);
+            outputForm.ShowDialog();
+        }
+    }
+}
